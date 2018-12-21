8953508aeee48b159dc9734eb078924e04b59125
Philippe Warnon <Philippe.Warnon@civadis.be>
2018-12-21 16:20:12 +0100

    call keycloak

--- src/main/java/be/phw/gedclient/client/document/config/DocumentMainFeignConfiguration.java
+++ src/main/java/be/phw/gedclient/client/document/config/DocumentMainFeignConfiguration.java
@@ -1,13 +1,22 @@
 package be.phw.gedclient.client.document.config;
 
 import feign.Contract;
+import feign.RequestInterceptor;
 import feign.form.FormEncoder;
 import feign.gson.GsonEncoder;
+
+import java.io.IOException;
+
 import org.springframework.cloud.openfeign.support.ResponseEntityDecoder;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 
-public class DocumentMainFeignConfiguration { // extends OAuth2InterceptedFeignConfiguration
+import be.phw.gedclient.client.GedRequestInterceptor;
+import be.phw.gedclient.config.ApplicationProperties;
+import be.phw.gedclient.security.oauth2.AuthorizationHeaderUtil;
+
+public class DocumentMainFeignConfiguration {
 
     @Bean
     public Contract feignContract() {
@@ -25,6 +34,11 @@ public class DocumentMainFeignConfiguration { // extends OAuth2InterceptedFeignC
     public feign.codec.Decoder feignDecoder() {
         //return new feign.codec.Decoder.Default();
         return new ResponseEntityDecoder(new CustomFileDecoder(new CustomGsonDecoder()));
+    }
+
+    @Bean(name = "gedRequestInterceptor")
+    public RequestInterceptor getGedRequestInterceptor(AuthorizationCodeResourceDetails authorizationCodeResourceDetails, ApplicationProperties applicationProperties) throws IOException {
+        return new GedRequestInterceptor(authorizationCodeResourceDetails, applicationProperties);
     }
 
 }
--- src/main/java/be/phw/gedclient/config/ApplicationProperties.java
+++ src/main/java/be/phw/gedclient/config/ApplicationProperties.java
@@ -10,5 +10,40 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+    private String gedUser;
+    private String gedPasword;
+
+    public ApplicationProperties() {
+    }
+
+    /**
+     * @return the gedPasword
+     */
+    public String getGedPasword() {
+        return gedPasword;
+    }
+
+    /**
+     * @param gedPasword the gedPasword to set
+     */
+    public void setGedPasword(String gedPasword) {
+        this.gedPasword = gedPasword;
+    }
+
+    /**
+     * @return the gedUser
+     */
+    public String getGedUser() {
+        return gedUser;
+    }
+
+    /**
+     * @param gedUser the gedUser to set
+     */
+    public void setGedUser(String gedUser) {
+        this.gedUser = gedUser;
+    }
+
+
 
 }
